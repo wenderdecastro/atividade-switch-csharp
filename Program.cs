99df8f41ba1aeeb82c7c4420be3fd0597aa19584
Wender <wendermaiac@gmail.com>
2023-04-04 08:33:46 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -16,20 +16,30 @@
 ");
 
 ConsoleKeyInfo inputOpcao = Console.ReadKey();
+string drink1 = "Água";
+string drink2 = "Refrigerante";
+string drink3 = "Drink alcólico";
+string drink4 = "Suco natural";
+
+string escolha = "Vidro";
 
 switch (inputOpcao.Key)
 {
     case ConsoleKey.D1:
-        Console.WriteLine($"Você selecionou a opção Água.");
+        Console.WriteLine($"Você selecionou a opção {drink1}.");
+        escolha = drink1;
         break;
     case ConsoleKey.D2:
-        Console.WriteLine($"Você selecionou a opção Refrigerante.");
+        Console.WriteLine($"Você selecionou a opção {drink2}.");
+        escolha = drink2;
         break;
     case ConsoleKey.D3:
-        Console.WriteLine($"Você selecionou a opção Drink alcólico");
+        Console.WriteLine($"Você selecionou a opção {drink3}");
+        escolha = drink3;
         break;
     case ConsoleKey.D4:
-        Console.WriteLine($"Você selecionou a opção Suco natural");
+        Console.WriteLine($"Você selecionou a opção {drink4}");
+        escolha = drink4;
         break;
     case ConsoleKey.D5:
         Console.WriteLine($"Tudo bem, tenha um bom dia.");
@@ -37,11 +47,12 @@ switch (inputOpcao.Key)
         break;
 }
 
-Console.WriteLine(@"
+Console.WriteLine(@$"
 -------------------------------------------------
 |             CARDÁPIO DE BEBIDAS               |
 -------------------------------------------------
-|            Deseja adicionar gelo?             |
+|     Deseja adicionar gelo no seu copo de      |
+|     {escolha}?                   
 -------------------------------------------------
 |                                               |
 |  S - Sim                                      |
@@ -52,13 +63,13 @@ Console.WriteLine(@"
 
 ConsoleKeyInfo desejaGelo = Console.ReadKey();
 
-switch (inputOpcao.Key)
+switch (desejaGelo.Key)
 {
     case ConsoleKey.S:
-        Console.WriteLine($"Tudo bem, adicionaremos gelo.");
+        Console.WriteLine($"Tudo bem, adicionaremos gelo no seu copo de {escolha}.");
         break;
     case ConsoleKey.N:
-        Console.WriteLine($"Tudo bem, não adicionaremos gelo.");
+        Console.WriteLine($"Tudo bem, não adicionaremos gelo no seu copo de {escolha}.");
         break;
     default:
         Console.WriteLine($"Você não inseriu uma resposta válida, não adicionaremos gelo.");
